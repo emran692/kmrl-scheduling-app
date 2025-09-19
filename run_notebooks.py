5a2120dc881af3297b0ed222bc4811dfb3cc94a5
MD AL EMRAN <alimarn.068@gmail.com>
2025-09-19 22:28:27 +0530

    Add files via upload

--- run_notebooks.py
+++ run_notebooks.py
@@ -1,21 +1,5 @@
-import subprocess
-
-# List of notebooks in order
-notebooks = [
-    "notebooks/01_data_analysis.ipynb",
-    "notebooks/02_model_training.ipynb",
-    "notebooks/03_optimization_engine.ipynb",
-    "notebooks/04_save_model.ipynb"
-]
-
-for nb in notebooks:
-    print(f"\n Running {nb} ...")
-    subprocess.run([
-        "jupyter", "nbconvert",
-        "--to", "notebook",
-        "--execute", nb,
-        "--inplace"
-    ])
-    print(f" Finished {nb}")
-
-print("\n All notebooks executed successfully!")
+import os 
+os.system("jupyter nbconvert --to notebook --execute notebooks/01_data_analysis.ipynb") 
+os.system("jupyter nbconvert --to notebook --execute notebooks/02_model_training.ipynb") 
+os.system("jupyter nbconvert --to notebook --execute notebooks/03_optimization_engine.ipynb") 
+os.system("jupyter nbconvert --to notebook --execute notebooks/04_save_model.ipynb") 
